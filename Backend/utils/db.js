2c2deecb44160cd492fe3b62f84358d2838c7b09
krgarav <krgarav@gmail.com>
2024-11-26 10:22:13 +0530

    PWD changed

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("torrentpower", "root", "root123@", {
+export const sequelize = new Sequelize("torrentpower", "root", "root", {
     dialect: "mysql",
     // host: "database",
     // port: 3307
