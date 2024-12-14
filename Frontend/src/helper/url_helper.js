f2f96b06fc854b8f9958223ad777c9abc2f56a08
krgarav <krgarav@gmail.com>
2024-12-14 14:30:39 +0530

    IP and port changed

--- Frontend/src/helper/url_helper.js
+++ Frontend/src/helper/url_helper.js
@@ -4,8 +4,8 @@
 // const url = "http://192.168.1.17:8000/"
 // export const url2 = "http://192.168.1.17:8000"
 
-const url = "http://localhost:4000/";
-export const url2 = "http://localhost:4000";
+const url = "http://10.144.6.12:3000/";
+export const url2 = "http://10.144.6.12:3000";
 
 export default url + "createUser";
 export const UPDATE_USER = url + "updateUser/";
